79d24dd64936a95f9c50eea352042dad46032875
Wedson Almeida Filho <wedsonaf@gmail.com>
2023-11-21 00:57:30 -0300

    Primeiro exemplo

---
+++ ola-2.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let i = 10;
+    let j: i32 = 20;
+    let s = "teste";
+
+    println!("Olá mundo: {i} {j} {s} {}", i + j);
+}
---
+++ ola-3.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let i = 10;
+    let mut j: i32 = 20;
+    let s = "teste";
+
+    j = i + j + 1;
+
+    println!("Olá mundo: {i} {j} {s} {}", i + j);
+}
